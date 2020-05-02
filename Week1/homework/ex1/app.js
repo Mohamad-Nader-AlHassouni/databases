7af13b1e3161d1190f200264faa2bc5efc6de140
Nader <Nader.Hassouni@Hotmail.com>
2020-05-02 23:37:07 +0200

    finished homework week1

---
+++ Week1/homework/ex1/app.js
@@ -0,0 +1,68 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+});
+
+connection.query("DROP DATABASE IF EXISTS meetup", function (err, result) {
+    if (err) throw err;
+    console.log("Database deleted");
+});
+
+connection.query("CREATE DATABASE meetup", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+});
+
+connection.query("USE meetup", function (err, result) {
+    if (err) throw err;
+    console.log("Database connected");
+});
+
+const create_tables = [
+  "create table Invitee (invitee_no int, invitee_name varchar(50),  invited_by varchar(50))",
+  "create table Room (room_no int, room_name varchar(50),  floor_number int)",
+  "create table Meeting (meeting_no int, meeting_title varchar(50),  starting_time TIME (0) , ending_time TIME (0) , room_no int )",
+];
+
+const insert_queries = [
+  //invitee table
+  "insert into Invitee values (1, 'Fede', 'Wouter')",
+  "insert into Invitee values (2, 'Tjebbe', 'Wouter')",
+  "insert into Invitee values (3, 'Noer', 'Wouter')",
+  "insert into Invitee values (4, 'Unmesh', 'Wouter')",
+  "insert into Invitee values (5, 'Igor', 'Wouter')",
+  //room table
+  "insert into room values (1, 'Amsterdam', 11)",
+  "insert into room values (2, 'The Hague', 12)",
+  "insert into room values (3, 'Rotterdam', 13)",
+  "insert into room values (4, 'Leiden', 14)",
+  "insert into room values (5, 'Utrecht', 15)",
+  //meeting table
+  "insert into meeting values (1, 'Class 27', '13:00' , '14:00', 1)",
+  "insert into meeting values (2, 'Class 26', '15:00' , '16:00', 2)",
+  "insert into meeting values (3, 'Class 25', '13:00' , '14:00', 3)",
+  "insert into meeting values (4, 'Class 24', '13:00' , '14:00', 4)",
+  "insert into meeting values (5, 'Class 23', '17:00' , '18:00', 5)",
+];
+
+for (let i in create_tables) {
+  connection.query(create_tables[i], function (error, results) {
+    if (error) {
+      throw error;
+    }
+    console.log("Table is created");
+});
+}
+
+for (let i in insert_queries) {
+  connection.query(insert_queries[i], function (error, results) {
+    if (error) {
+      throw error;
+    }
+    console.log("Value is inserted");
+  });
+}
+
+connection.end();
---
+++ Week1/homework/ex2/index.js
@@ -0,0 +1,42 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "hyfuser",
+  password: "hyfpassword",
+  database: "new_world",
+});
+
+connection.connect();
+
+const queryObj = {
+  countries_with_population_greater_than_8_million:
+    "select name From country where Population > 8000000 ",
+  countries_that_have_land_in_their_names:
+    'select name From country where name like  "%land%"',
+  the_cities_with_population_in_between_500000_and_a_million:
+    "select name From country where population between 500000 and 1000000",
+  all_the_countries_on_the_continent_Europe:
+    "select name From country where continent = 'Europe'",
+  the_countries_in_the_descending_order_of_their_surface_areas:
+    "select name  FROM country ORDER BY SurfaceArea DESC",
+  all_the_cities_in_the_Netherlands:
+    "SELECT name From city where countryCode = 'NLD'",
+  population_of_Rotterdam:
+    "SELECT Population from city where name = 'Rotterdam'",
+  biggest_10_countries:
+    "select name FROM country ORDER BY SurfaceArea DESC limit 10",
+  biggest_10_cities: 
+    "select name FROM city ORDER BY Population DESC limit 10",
+  population_number_of_the_world: 
+    "select sum (population) from country",
+};
+
+for (let i in queryObj)
+  connection.query(queryObj[i], function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log(`${i}`, results);
+  });
+
+connection.end();
